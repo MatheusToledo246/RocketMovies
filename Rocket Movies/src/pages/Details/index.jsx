ef81e351e5a016e5b20cd3666a17e214fbf04311
Matheus <matheus.silva246@fatec.sp.gov.br>
2024-02-18 14:40:02 -0300

    add button

--- Rocket Movies/src/pages/Details/index.jsx
+++ Rocket Movies/src/pages/Details/index.jsx
@@ -1,10 +1,16 @@
 import { Container } from './styles';
 
+import { Button } from '../../components/Button';
+
 export function Details() {
   return(
     <Container>
       <h1>Para de dar erro</h1>
       <span>Matheus Toledo</span>
+
+      <Button title="Cadastras"/>
+      <Button title="logar"/>
+      <Button title="voltar" />
     </Container>
   )
 }
--- Rocket Movies/src/styles/theme.js
+++ Rocket Movies/src/styles/theme.js
@@ -1,7 +1,8 @@
 export default {
     COLORS: {
-      BACKGROUND_900: "#1C1B1E",
-      BACKGROUND_700: "#262529",
+      BACKGROUND_900: "#312E38",
+      BACKGROUND_700: "#1C1B1E",
+      BACKGROUND_500: "#262529",
       BACKGROUND_100: "#282124",
       BACKGROUND_BUTTON: "0D0C0F",
       
